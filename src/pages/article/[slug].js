2769afdb29cb4f1ef41e63b2a488f0848948cc6e
Sofia L <euynil@applemacdembp.myfiosgateway.com>
2023-09-28 14:08:27 -0400

    changes made on sep 28

--- src/pages/article/[slug].js
+++ src/pages/article/[slug].js
@@ -1,7 +1,8 @@
 //within js you can export and import code
 import { useRouter } from "next/router";
+import "../../app/globals.css"; //serve as a reset stylesheet
 import Data from '../../app/components/data';
-import styles from '../../app/page.module.css';
+import styles from './article.module.css';
 
 export default function Article() {
     const router = useRouter();
@@ -13,14 +14,38 @@ export default function Article() {
     if(!articleData) return null; //sometimes do this for loading page
 
     return (
-        <main className={styles.page}>
-            <h1>Article</h1>
-            <p>{articleData.title}</p>
-            <p>{articleData.blurb}</p>
-            <p>{articleData.articleText}</p>
-            <div>
-                {Data.map((textChunk)) => <p>{textChunk.}</p>}
+        <main> 
+            
+            <div className={styles.articleHeader} style={ {backgroundImage: `url('${articleData.image.url}')`} }>
+                <div className={styles.articleSection}>
+                    {/* <h1>Article</h1> */}
+                    <h1>{articleData.title}</h1>
+                    <p>{new Date(articleData.publishedDate).toDateString()}</p>
+                    <p className={styles.articleHeaderBlurb}>{articleData.blurb}</p>
+                    {/* <p>{articleData.articleText}</p> */}
+                </div>
             </div>
+
+            <div className={styles.articleBody}>
+                <div className={styles.articleSection}>
+                    {/* {Data.map((textChunk)) => <p>{textChunk.}</p>} */}
+                    
+                    {articleData.articleText.map((textChunk) => {
+                        switch(textChunk.type){
+                            case 'p':
+                                return <p>{textChunk.data}</p>
+                            case 'h2':
+                                return <h2>{textChunk.data}</h2>
+                            case 'h3':
+                                return <h3>{textChunk.data}</h3>
+                            default:
+                                return <p>{textChunk.data}</p>
+                        }
+                    })}
+
+                </div>
+            </div>
+            
         </main>
     );
 }
